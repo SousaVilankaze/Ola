5c475ef02507b5686571bdebc494a752538cefad
Sousa Vilankaze <87579657+SousaVilankaze@users.noreply.github.com>
2024-01-13 00:19:51 +0000

    Site

---
+++ index.js
@@ -0,0 +1,16 @@
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  axios.post("https://reqres.in/api/login", {
+      email: email,
+      password: password
+    })
+    .then((response) => {
+      console.log(response);
+    });
+});
